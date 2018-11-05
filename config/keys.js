90353fa4e36babc183f9099281dd764fcca2d64f
hzs90 <zsolt.huszar@live.com>
2018-11-05 19:56:16 +0100

    JWT added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:qweQWE123@ds151523.mlab.com:51523/devconnector_hzs'
+  mongoURI: 'mongodb://brad:qweQWE123@ds151523.mlab.com:51523/devconnector_hzs',
+  secretOrKey: 'secret'
 }
